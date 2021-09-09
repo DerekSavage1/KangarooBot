2e75215d21b92c8f4565cba3c31bc5c680ed239f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-09 08:47:59 -0400

    Added Death HashMap

--- src/main/java/me/ChewyN/Minecraft/Listeners/Player/PlayerDeath.java
+++ src/main/java/me/ChewyN/Minecraft/Listeners/Player/PlayerDeath.java
@@ -2,20 +2,27 @@ package me.ChewyN.Minecraft.Listeners.Player;
 
 import me.ChewyN.Minecraft.Util.Message;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+
 import static me.ChewyN.Main.getInstance;
 
 public class PlayerDeath implements Listener {
+
+	private HashMap<Player, Location> deathMap = new HashMap<>();
 
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent e) {
 		e.setDeathMessage(null); // hmm will this throw an error? FIXME
 		Message.sendCenteredMessage(e.getEntity().getServer(), ChatColor.RED + "☠ " + ChatColor.WHITE + e.getEntity().getPlayerListName() +  " has died"  + ChatColor.RED + " ☠");
 
+		deathMap.put(e.getEntity(), e.getEntity().getLocation());
 
 		new BukkitRunnable(){
 			@Override
