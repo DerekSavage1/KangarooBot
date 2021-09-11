63af50f63c960264a9dd081d8e27fbd4d1386aac
dsava <d.savag1@gmail.com>
2021-09-10 20:23:28 -0700

    admins get messages from admin discord

--- src/main/java/me/ChewyN/Discord/Listeners/onChat.java
+++ src/main/java/me/ChewyN/Discord/Listeners/onChat.java
@@ -6,10 +6,17 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import static me.ChewyN.Main.discordbot;
+
+import static me.ChewyN.Main.getInstance;
+
 import me.ChewyN.Data.ConfigFile;
+
+import java.util.Collection;
+import java.util.List;
 
 public class onChat extends ListenerAdapter {
 
@@ -23,10 +30,18 @@ public class onChat extends ListenerAdapter {
 
         if(e.getChannel().equals(GAME_TEXT_CHANNEL)) {
             if(!user.isBot()) {
-                Main.getInstance().getServer().broadcastMessage("[" + ChatColor.AQUA + "Discord" + ChatColor.WHITE + "] " + ChatColor.AQUA + userName + ": " + ChatColor.WHITE + DMessage);
+                getInstance().getServer().broadcastMessage("[" + ChatColor.AQUA + "Discord" + ChatColor.WHITE + "] " + ChatColor.AQUA + userName + ": " + ChatColor.WHITE + DMessage);
 
                 assert ADMIN_TEXT_CHANNEL != null;
                 ADMIN_TEXT_CHANNEL.sendMessage("[Discord] " + userName + ": " + DMessage).queue();
+            }
+        }
+
+        if(e.getChannel().equals(ADMIN_TEXT_CHANNEL)) {
+            for(Player p : getInstance().getServer().getOnlinePlayers()) {
+                if(p.hasPermission("adminChat.see")) {
+                    p.sendMessage("[" + ChatColor.RED + "Admin " + ChatColor.WHITE + "] " + ChatColor.RED + userName + ": " + ChatColor.WHITE + DMessage);
+                }
             }
         }
 
