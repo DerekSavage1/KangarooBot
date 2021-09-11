742103211f672875ae1c375f87abf09ecd16ba66
Skyla <skylaclark094@gmail.com>
2021-09-11 04:41:11 -0400

    Grappling hook now has a 10s timer between use. Used player metadata... :)

--- src/main/java/me/ChewyN/Minecraft/Commands/GrapplingHook.java
+++ src/main/java/me/ChewyN/Minecraft/Commands/GrapplingHook.java
@@ -1,27 +1,37 @@
 package me.ChewyN.Minecraft.Commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import me.ChewyN.Main;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+//import java.util.HashMap;
 
 public class GrapplingHook extends AbstractCommand implements CommandExecutor {
 
+    // Grapple Cooldown HashMap
+    //private HashMap<Player, Integer> coolMap = new HashMap<Player, Integer>();
+
     @Override
     public boolean onCommand(@NotNull CommandSender s, Command cmd, @NotNull String label, String[] args) {
-        if(cmd.getName().equals("grapplinghook")) {
-            if(s instanceof Player) {
+        if (cmd.getName().equals("grapplinghook")) {
+            // Check if player
+            if (s instanceof Player) {
                 Player sender = (Player) s;
-                executeCommand(sender);
+                // Check if cooldown exists
+                if (!sender.hasMetadata("grapple_cooldown")) {
+                    executeCommand(sender);
+                    return true;
+                }
+                sender.sendMessage(ChatColor.RED + "Command on cooldown!");
                 return true;
             } else {
                 s.sendMessage("Only players may run this command!");
@@ -34,8 +44,7 @@ public class GrapplingHook extends AbstractCommand implements CommandExecutor {
     }
 
     private void executeCommand(Player p) {
-
-        if(p.getInventory().firstEmpty() == -1) {
+        if (p.getInventory().firstEmpty() == -1) {
             //inventory is full
             Location loc = p.getLocation();
             World world = p.getWorld();
@@ -43,11 +52,15 @@ public class GrapplingHook extends AbstractCommand implements CommandExecutor {
         } else {
             p.getInventory().addItem(getGrapplingHook());
         }
+        // Add the cooldown and remove it after 200 ticks (10s)
+        p.setMetadata("grapple_cooldown", new FixedMetadataValue(Main.getInstance(), System.currentTimeMillis()));
+        BukkitScheduler sch = Main.getInstance().getServer().getScheduler();
+        sch.scheduleSyncDelayedTask(Main.getInstance(), () -> p.removeMetadata("grapple_cooldown", Main.getInstance()), 200L);
     }
 
     private ItemStack getGrapplingHook() {
-        ItemStack   grapple = new ItemStack(Material.FISHING_ROD);
-        ItemMeta    meta = grapple.getItemMeta();
+        ItemStack grapple = new ItemStack(Material.FISHING_ROD);
+        ItemMeta meta = grapple.getItemMeta();
 
         assert meta != null;
         meta.setDisplayName(ChatColor.GOLD + "Grappling Hook");
