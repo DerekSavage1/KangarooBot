1ef1d1ec1169813d5dd199cf4a5d425e93ace5e7
dsava <d.savag1@gmail.com>
2021-09-11 02:22:06 -0700

    updated abstract commands

--- src/main/java/me/ChewyN/Minecraft/Commands/AbstractCommand.java
+++ src/main/java/me/ChewyN/Minecraft/Commands/AbstractCommand.java
@@ -66,8 +66,6 @@ public abstract class AbstractCommand implements CommandExecutor {
      * Sets the command sender to a player. Use only if the sender is a player.
      * @param s The command sender
      */
-    public void setSender(CommandSender s) {
-        this.s = s;
     public void setCommandSender(CommandSender s) {
         this.commandSender = s;
         if(s instanceof Player)
