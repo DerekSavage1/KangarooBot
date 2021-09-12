4ddae9f292993e9fbfbed4b00a429961d0ebae93
dsava <d.savag1@gmail.com>
2021-09-12 13:47:41 -0700

    discord image url sent to minecraft

--- src/main/java/me/ChewyN/Discord/Listeners/onChat.java
+++ src/main/java/me/ChewyN/Discord/Listeners/onChat.java
@@ -1,6 +1,7 @@
 package me.ChewyN.Discord.Listeners;
 
-import me.ChewyN.Main;
+import me.ChewyN.Data.ConfigFile;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -10,18 +11,12 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import static me.ChewyN.Main.discordbot;
-
 import static me.ChewyN.Main.getInstance;
-
-import me.ChewyN.Data.ConfigFile;
-
-import java.util.Collection;
-import java.util.List;
 
 public class onChat extends ListenerAdapter {
 
     public void onMessageReceived(@NotNull MessageReceivedEvent e) {
-        String      DMessage = e.getMessage().getContentRaw();
+        Message     discordMessage = e.getMessage();
         User        user = e.getAuthor();
         String      userName = user.getName();
 
@@ -30,10 +25,12 @@ public class onChat extends ListenerAdapter {
 
         if(e.getChannel().equals(GAME_TEXT_CHANNEL)) {
             if(!user.isBot()) {
-                getInstance().getServer().broadcastMessage("[" + ChatColor.AQUA + "Discord" + ChatColor.WHITE + "] " + ChatColor.AQUA + userName + ": " + ChatColor.WHITE + DMessage);
+                String formattedMessage = formatMessage(e.getMessage());
+
+                getInstance().getServer().broadcastMessage("[" + ChatColor.AQUA + "Discord" + ChatColor.WHITE + "] " + ChatColor.AQUA + userName + ": " + ChatColor.WHITE + formattedMessage);
 
                 assert ADMIN_TEXT_CHANNEL != null;
-                ADMIN_TEXT_CHANNEL.sendMessage("[Discord] " + userName + ": " + DMessage).queue();
+                ADMIN_TEXT_CHANNEL.sendMessage("[Discord] " + userName + ": " + formattedMessage).queue();
             }
         }
 
@@ -42,12 +39,27 @@ public class onChat extends ListenerAdapter {
             if(!e.getAuthor().isBot()) {
                 for(Player p : getInstance().getServer().getOnlinePlayers()) {
                     if(p.hasPermission("adminChat.see")) {
-                        p.sendMessage("[" + ChatColor.RED + "Admin" + ChatColor.WHITE + "] " + ChatColor.RED + userName + ": " + ChatColor.WHITE + DMessage);
+                        p.sendMessage("[" + ChatColor.RED + "Admin" + ChatColor.WHITE + "] " + ChatColor.RED + userName + ": " + ChatColor.WHITE + discordMessage);
                     }
                 }
             }
         }
 
+
+
+    }
+    private String formatMessage(Message message) {
+
+        if(message.getAttachments().size() > 1) {
+            return "(multiple attachments) " + message.getJumpUrl();
+        }
+
+        if(message.getAttachments().get(0).isImage()) {
+            Message.Attachment image = message.getAttachments().get(0);
+            return image.getUrl();
+        }
+
+        return message.toString();
 
     }
 
