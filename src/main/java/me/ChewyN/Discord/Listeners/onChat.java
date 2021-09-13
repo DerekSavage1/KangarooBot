dab55ede6498b917bfde5fe0e1aa83076903bf2e
dsava <d.savag1@gmail.com>
2021-09-12 18:13:05 -0700

    added adminChannelEnabled in config

--- src/main/java/me/ChewyN/Discord/Listeners/onChat.java
+++ src/main/java/me/ChewyN/Discord/Listeners/onChat.java
@@ -1,8 +1,8 @@
 package me.ChewyN.Discord.Listeners;
 
 import me.ChewyN.Data.ConfigFile;
+import me.ChewyN.Main;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -22,23 +22,21 @@ public class onChat extends ListenerAdapter {
         User        user = e.getAuthor();
         String      userName = user.getName();
 
-        final TextChannel   GAME_TEXT_CHANNEL = ConfigFile.getMinecraftChannel(discordbot);
-        final TextChannel   ADMIN_TEXT_CHANNEL = ConfigFile.getAdminChannel(discordbot);
+        if(user.isBot())
+            return;
 
-        if(e.getChannel().equals(GAME_TEXT_CHANNEL)) {
-            if(!user.isBot()) {
-                String formattedMessage = formatMessage(e.getMessage());
+        if(e.getChannel().equals(ConfigFile.getMinecraftChannel(discordbot))) {
+            String formattedMessage = formatMessage(e.getMessage());
 
-                getInstance().getServer().broadcastMessage("[" + ChatColor.AQUA + "Discord" + ChatColor.WHITE + "] " + ChatColor.AQUA + userName + ": " + ChatColor.WHITE + formattedMessage);
+            getInstance().getServer().broadcastMessage("[" + ChatColor.AQUA + "Discord" + ChatColor.WHITE + "] " + ChatColor.AQUA + userName + ": " + ChatColor.WHITE + formattedMessage);
 
-                assert ADMIN_TEXT_CHANNEL != null;
-                ADMIN_TEXT_CHANNEL.sendMessage("[Discord] " + userName + ": " + formattedMessage).queue();
+            if(!Main.getConfigFile().isAdminChannelEnabled() && ConfigFile.getAdminChannel(discordbot) != null) {
+                ConfigFile.getAdminChannel(discordbot).sendMessage("[Discord] " + userName + ": " + formattedMessage).queue();
             }
+
         }
-
-        if(e.getChannel().equals(ADMIN_TEXT_CHANNEL)) {
-
-            if(!e.getAuthor().isBot()) {
+        if(!Main.getConfigFile().isAdminChannelEnabled() && ConfigFile.getAdminChannel(discordbot) != null) {
+            if(e.getChannel().equals(ConfigFile.getAdminChannel(discordbot))) {
                 for(Player p : getInstance().getServer().getOnlinePlayers()) {
                     if(p.hasPermission("adminChat.see")) {
                         p.sendMessage("[" + ChatColor.RED + "Admin" + ChatColor.WHITE + "] " + ChatColor.RED + userName + ": " + ChatColor.WHITE + discordMessage);
@@ -46,6 +44,7 @@ public class onChat extends ListenerAdapter {
                 }
             }
         }
+
 
 
 
