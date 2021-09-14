b0441cac65dae822bbafb18d39e79bcc102c1f43
dsava <d.savag1@gmail.com>
2021-09-14 12:26:38 -0700

    added an exception command so when I am testing out my new thing I don't have to embarrass myself by telling the entire server about my love for cum

---
+++ src/main/java/me/ChewyN/Minecraft/Commands/ExceptionCommand.java
@@ -0,0 +1,25 @@
+package me.ChewyN.Minecraft.Commands;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ExceptionCommand extends AbstractCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) throws NullPointerException {
+
+        if(isCommandEnabled()) {
+            sender.sendMessage(ChatColor.RED + "Command is disabled.");
+            return true;
+        }
+
+        if(!sender.isOp())
+            return true;
+
+        throw new NullPointerException("Exception Command");
+
+    }
+}
